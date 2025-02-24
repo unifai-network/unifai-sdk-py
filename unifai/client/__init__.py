e24cebcf85830877b4505130b7d658bee2b7f322
Yilun <zyl.skysniper@gmail.com>
2025-02-24 05:49:02 -0800

    make BaseClient pure abstract
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

---
+++ unifai/client/__init__.py
@@ -0,0 +1,4 @@
+from .base import BaseClient
+from .telegram import TelegramClient
+
+__all__ = ["BaseClient", "TelegramClient"]
--- unifai/client/base.py
+++ unifai/client/base.py
@@ -1,18 +1,5 @@
 from abc import ABC, abstractmethod
 from typing import Dict, Any, Optional
-from functools import wraps
-import asyncio
-import logging
-
-logger = logging.getLogger(__name__)
-
-def ensure_started(func):
-    @wraps(func)
-    async def wrapper(self, *args, **kwargs):
-        if not self._started:
-            raise RuntimeError(f"Client {self.client_id} not started")
-        return await func(self, *args, **kwargs)
-    return wrapper
 
 class MessageContext(ABC):
     @property
@@ -40,34 +27,28 @@ class MessageContext(ABC):
         pass
 
 class BaseClient(ABC):
-    def __init__(self, client_id: str):
-        self._client_id = client_id
-        self._started = False
-        self._message_queue = asyncio.Queue()
-        self._stop_event = asyncio.Event()
-
     @property
+    @abstractmethod
     def client_id(self) -> str:
-        return self._client_id
+        """Get client ID"""
+        raise NotImplementedError("client_id is not implemented")
 
     @abstractmethod
     async def start(self):
         """Start the client"""
-        pass
+        raise NotImplementedError("start is not implemented")
 
     @abstractmethod
     async def stop(self):
         """Stop the client"""
-        pass
+        raise NotImplementedError("stop is not implemented")
 
     @abstractmethod
-    @ensure_started
     async def receive_message(self) -> Optional[MessageContext]:
         """Receive a message from the queue"""
-        pass
+        raise NotImplementedError("receive_message is not implemented")
 
     @abstractmethod
-    @ensure_started
     async def send_message(self, ctx: MessageContext, reply: str):
         """Send a message using the context"""
-        pass
+        raise NotImplementedError("send_message is not implemented")
